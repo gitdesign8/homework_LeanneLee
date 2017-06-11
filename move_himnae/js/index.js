1adeae278aded67cf0ddf732649300ee5efd677d
gitdesign8 <leanne@leanne-lee.com>
2017-06-10 22:49:19 -0700

    Add files via upload

---
+++ move_himnae/js/index.js
@@ -0,0 +1,10 @@
+
+$( '#greenButton' ).click(function() {
+  $( ".himnae2" ).animate({ ".himnae2": "+=50px" }, "slow" );
+});
+
+$( "#yelloButton" ).click(function(){
+  $( ".himnae2" ).animate({ ".himnae2": "-=50px" }, "slow" );
+});
+	
+
