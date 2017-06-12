6b9f02d87b7a73987d40815b46c581b6628728f5
Jonathan Boswell <jonathan@arborbridge.com>
2017-06-12 13:38:09 -0700

    Updates :)

--- move_himnae/js/index.js
+++ move_himnae/js/index.js
@@ -1,6 +1,7 @@
 
-$( '#greenButton' ).click(function() {
-  $( ".himnae2" ).animate({ ".himnae2": "+=50px" }, "slow" );
+$('#greenButton').click(function() {
+    console.log("here");
+    $("#himnae").animate({ left: "+=100", opacity:".25" }, 500);
 });
 
 $( "#yelloButton" ).click(function(){
